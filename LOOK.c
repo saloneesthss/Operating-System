b468c4357131347832c8188c7a1fb990bfe51106
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-12-11 18:44:42 +0545

    added disk codes

---
+++ LOOK.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, j, n, head, total = 0;
+    int queue[20], left[20], right[20];
+
+    printf("Enter the size of disk queue: ");
+    scanf("%d", &n);
+
+    printf("Enter the disk queue: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &queue[i]);
+    }
+   
+    printf("Enter the initial head position: ");
+    scanf("%d", &head);
+
+    // sort the queue
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (queue[i] > queue[j]) {
+                int temp = queue[i];
+                queue[i] = queue[j];
+                queue[j] = temp;
+            }
+        }
+    }
+
+    // find the left and right end of disk
+    for (i = 0; i < n; i++) {
+        if (queue[i] < head) {
+            left[i] = 1;
+        } else {
+            left[i] = 0;
+        }
+        if (queue[i] > head) {
+            right[i] = 1;
+        } else {
+            right[i] = 0;
+        }
+    }
+
+    int seek_count = 0;
+    int direction;
+    printf("%d \n", direction);
+    printf("Enter the direction (0 for left and 1 for right): ");
+    scanf("%d", &direction);
+
+    // if direction is right
+    if (direction == 1) {
+        for (i = 0; i < n; i++) {
+            if (right[i] == 1) {
+                printf("%d -> ", queue[i]);
+
+                seek_count += abs(head - queue[i]);
+
+                head = queue[i];
+            }
+        }
+       
+        for (i = n - 1; i >= 0; i--) {
+            if (left[i] == 1) {
+                printf("%d -> ", queue[i]);
+                seek_count += abs(head - queue[i]);
+                head = queue[i];
+            }
+        }
+    }
+
+    // if direction is left
+    else {
+        for (i = n - 1; i >= 0; i--) {
+            if (left[i] == 1) {
+                printf("%d -> ", queue[i]);
+
+                seek_count += abs(head - queue[i]);
+                head = queue[i];
+            }
+        }
+        
+        for (i = 0; i < n; i++) {
+            if (right[i] == 1) {
+                printf("%d -> ", queue[i]);
+                seek_count += abs(head - queue[i]);
+                head = queue[i];
+            }
+        }
+    }
+
+    printf("\nTotal seek count = %d\n", seek_count);
+    return 0;
+}
